0cfb78e869b0614398f12d40ec9c8cec74ae219a
Ariful Alam <alamariful1727@gmail.com>
2024-03-02 12:04:14 -0500

    feat: project initialized

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-slate-600">
+      <h1 className="text-3xl font-bold text-white underline">
+        tax-calculator
+      </h1>
+    </div>
+  );
+}
+
+export default App;
